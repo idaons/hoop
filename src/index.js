d865a91b6a0b99720ff45480d1d92b880dbd1ce7
Ida Onshus <idaonshus@gmail.com>
2018-02-05 20:50:20 +0100

    prøver å fikse ghpages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/hoop">
         <App />
     </BrowserRouter>,
     document.getElementById('root'));
--- src/modules/Home.js
+++ src/modules/Home.js
@@ -10,8 +10,8 @@ class Home extends React.Component {
     };
     render() {
         return (
-            <div className="container">
-                <div className=" showcase">
+            <div className="container home-container">
+                <div className="showcase">
                     <RotatingHoops />
                 </div>
             </div>
