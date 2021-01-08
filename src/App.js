f3fa75a72b65d9f09db6549257a7a5db8ded41a9
Ida Onshus <Ida.Onshus@nav.no>
2021-01-08 15:33:50 +0100

    oppdaterer pakker

--- src/App.js
+++ src/App.js
@@ -1,64 +1,61 @@
-import React, { Component } from 'react';
-import './less/app.css'
+import React, { Component } from "react";
+import "./app.css";
 import Menu from "./modules/menu/Menu";
 import Trick from "./modules/Trick";
-import {
-    Route,
-    Switch
-} from 'react-router-dom';
-import ReactGA from 'react-ga';
-import Home from "./modules/Home"
-
+import { Route, Switch } from "react-router-dom";
+import ReactGA from "react-ga";
+import Home from "./modules/Home";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    ReactGA.initialize("UA-113167122-1");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    this.state = {
+      menuOpen: false,
+    };
+  }
 
-    constructor(props){
-        super(props);
-        ReactGA.initialize('UA-113167122-1');
-        ReactGA.pageview(window.location.pathname + window.location.search);
-        this.state = {
-          menuOpen: false
-        };
-    }
+  toggleMenu() {
+    this.setState({
+      menuOpen: !this.state.menuOpen,
+    });
+  }
 
-    toggleMenu(){
-        this.setState({
-            menuOpen: !this.state.menuOpen
-        });
-    }
+  openMenu() {
+    console.log("openMenu");
+    this.setState({
+      menuOpen: true,
+    });
+  }
 
-
-    openMenu(){
-        console.log("openMenu");
-        this.setState({
-            menuOpen: true
-        });
-    }
-
-
-    render() {
-        return (
-            <div className="app">
-                <div className="navigation-menu">
-                    <Menu open={this.state.menuOpen} callback={()=>this.toggleMenu()}/>
-                </div>
-                <div className="main-content">
-                    <Switch>
-                        {/*<Route exact path="/" component={() => <Redirect to="/triks/0" />}/>*/}
-                        <Route path='/triks/:id' component={Trick}/>
-                        <Route exact path='/' render={(props) => (
-                            <Home {...props} openMenu={()=>this.openMenu()} />
-                        )}/>
-                    </Switch>
-                </div>
-                <footer className="footer">
-                    <div className="container">
-                        <h4>App by Ida and Daniel</h4>
-                    </div>
-                </footer>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <div className="navigation-menu">
+          <Menu open={this.state.menuOpen} callback={() => this.toggleMenu()} />
+        </div>
+        <div className="main-content">
+          <Switch>
+            {/*<Route exact path="/" component={() => <Redirect to="/triks/0" />}/>*/}
+            <Route path="/triks/:id" component={Trick} />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <Home {...props} openMenu={() => this.openMenu()} />
+              )}
+            />
+          </Switch>
+        </div>
+        <footer className="footer">
+          <div className="container">
+            <h4>App by Ida and Daniel</h4>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/modules/RotatingHoops/RotatingHoops.js
+++ src/modules/RotatingHoops/RotatingHoops.js
@@ -1,18 +1,101 @@
-import React from 'react';
-import HulaBW from '../../img/hoop.svg';
-import ReactSVG from 'react-svg';
+import React from "react";
+import HulaBW from "../../img/hoop.svg";
+import { ReactSVG } from "react-svg";
+import styled from "styled-components/macro";
+
+const RotatingHoopsStyle = styled.div`
+  & > * {
+    position: absolute;
+  }
+  .rotate {
+    margin-top: 10px;
+    width: 100%;
+    @keyframes App-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+    animation: App-logo-spin infinite 240s linear;
+    svg {
+      width: 100%;
+      stroke: darken(@tertiary, 30%);
+      fill: transparent;
+    }
+  }
+  .rotate2 {
+    top: -50%;
+    left: -50%;
+    width: 400%;
+    @keyframes App-logo-spin2 {
+      from {
+        transform: rotate(360deg);
+      }
+      to {
+        transform: rotate(0deg);
+      }
+    }
+    animation: App-logo-spin2 infinite 2000s linear;
+    svg {
+      width: 100%;
+      stroke: darken(@tertiary, 40%);
+      fill: transparent;
+    }
+  }
+`;
+
+const RotateStyle = styled.div`
+  margin-top: 10px;
+  width: 100%;
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  animation: App-logo-spin infinite 240s linear;
+  svg {
+    width: 100%;
+    stroke: darken(@tertiary, 30%);
+    fill: transparent;
+  }
+`;
+
+const RotateStyle2 = styled.div`
+  top: -50%;
+  left: -50%;
+  width: 400%;
+  @keyframes App-logo-spin2 {
+    from {
+      transform: rotate(360deg);
+    }
+    to {
+      transform: rotate(0deg);
+    }
+  }
+  animation: App-logo-spin2 infinite 2000s linear;
+  svg {
+    width: 100%;
+    stroke: darken(@tertiary, 40%);
+    fill: transparent;
+  }
+`;
 
 function RotatingHoops(props) {
-    return (
-        <div className="rotating-hoops">
-            <div className="rotate2">
-                <ReactSVG path={HulaBW} />
-            </div>
-            <div className="rotate">
-                <ReactSVG path={HulaBW} />
-            </div>
-        </div>
-    );
+  return (
+    <RotatingHoopsStyle>
+      <RotateStyle2>
+        <ReactSVG src={HulaBW} />
+      </RotateStyle2>
+      <RotateStyle>
+        <ReactSVG src={HulaBW} />
+      </RotateStyle>
+    </RotatingHoopsStyle>
+  );
 }
 
 export default RotatingHoops;
